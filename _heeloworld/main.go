0d251d86335551b52fc9cc0e70f0fbc9b1a29820
Imran Khan Rafa <emailofimran1992@gmail.com>
2025-05-28 04:38:34 +0600

    IF EL.SE

--- _heeloworld/main.go
+++ _heeloworld/main.go
@@ -9,12 +9,27 @@ func main() {
 	fmt.Println("hello world " + name)
 
 	// loop
+	fmt.Println("LOOP")
 	for i := 1; i < 3; i++ {
 		fmt.Println(i)
 	}
 
+	fmt.Println("Range")
 	// range
 	for i := range 3 {
 		fmt.Println(i)
 	}
+
+	fmt.Println("IF ELSE")
+
+	age := 116
+
+	if age <= 16 {
+		fmt.Println("KID")
+	} else if age <= 20 {
+		fmt.Println("ADULT")
+	} else {
+		fmt.Println("ELDER")
+	}
+
 }
