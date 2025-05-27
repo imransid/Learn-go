6619ef5319720b5d31ee07c27fb609e492bec838
Imran Khan Rafa <emailofimran1992@gmail.com>
2025-05-28 04:05:36 +0600

    first commit

---
+++ _heeloworld/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "imran khan rafa" // shorthand
+
+	fmt.Println("hello world " + name)
+
+
+	
+
+	for i := 1; i < 3; i++ {
+		fmt.Println(i)
+	}
+}
